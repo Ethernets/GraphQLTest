dfc772952432de5fa98a1989d84dc2d653ea6de1
Ethernets <ethernetss@gmail.com>
2020-09-17 18:20:36 +0300

    GraphQl test v0.1

---
+++ test/test/WeatherForecastType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class WeatherForecastType : ObjectGraphType<WeatherForecast>
+    {
+        public WeatherForecastType()
+        {
+            Name = "Test";
+            Field(_ => _.Summary);
+            Field(_ => _.TemperatureC);
+            Field(_ => _.TemperatureF);
+            Field(_ => _.Date);
+        }
+    }
+}
